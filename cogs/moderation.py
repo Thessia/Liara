fe3fbd770c207b08557a2de256aa625131276cbb
Pandentia <hi@pandentia.cf>
2017-02-06 20:54:13 +0100

    Add moderation commands

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from cogs.utils import checks
 import discord
 import datetime
 
@@ -9,6 +10,11 @@ class Moderation:
 
     @commands.command(pass_context=True, no_pm=True)
     async def userinfo(self, ctx, user: discord.Member=None):
+        """Shows you a user's info.
+
+        Defaults to message author if user is not specified.
+        """
+
         if user is None:
             user = ctx.message.author
 
@@ -24,7 +30,9 @@ class Moderation:
 
         embed = discord.Embed()
         embed.title = '{} {}'.format(status, user)
-        embed.description = '**Display name**: {0.display_name}\n**ID**: {0.id}\n[Avatar]({0.avatar_url})'.format(user)
+        avatar_url = user.avatar_url.replace('webp', 'png')
+        embed.description = '**Display name**: {0.display_name}\n**ID**: {0.id}\n[Avatar]({1})'\
+                            .format(user, avatar_url)
 
         join_delta = datetime.datetime.now() - user.joined_at
         created_delta = datetime.datetime.now() - user.created_at
@@ -35,14 +43,15 @@ class Moderation:
         if roles:  # only show roles if the member has any
             if len(str(roles)) < 1025:  # deal with limits
                 embed.add_field(name='Roles', value=', '.join(roles))
-        embed.set_thumbnail(url=user.avatar_url)
+        embed.set_thumbnail(url=avatar_url.replace('size=1024', 'size=256'))
         try:
             await self.liara.say(embed=embed)
         except discord.HTTPException:
             await self.liara.say('Unable to post userinfo, please allow the Embed Links permission')
 
-    @commands.command(pass_context=True)
+    @commands.command(pass_context=True, no_pm=True)
     async def serverinfo(self, ctx):
+        """Shows you the server's info."""
         server = ctx.message.server
 
         if server.large:
@@ -88,6 +97,41 @@ class Moderation:
             await self.liara.say(embed=embed)
         except discord.HTTPException:
             await self.liara.say('Unable to post serverinfo, please allow the Embed Links permission')
+
+    @commands.command(no_pm=True)
+    @checks.mod_or_permissions(ban_members=True)
+    async def ban(self, member: discord.Member):
+        """Bans a member."""
+        try:
+            await self.liara.ban(member)
+            await self.liara.say('Done. Good riddance.')
+        except discord.Forbidden:
+            await self.liara.say('Sorry, I don\'t have permission to ban people here.')
+
+    @commands.command(no_pm=True)
+    @checks.mod_or_permissions(kick_members=True)
+    async def softban(self, member: discord.Member, days_to_clean: int=1):
+        """Kicks a member, removing all their messages in the process."""
+        if not 0 <= days_to_clean <= 7:
+            await self.liara.say('Invalid clean value. Use a number from 0 to 7 (days).')
+            return
+        try:
+            await self.liara.ban(member, days_to_clean)
+            await self.liara.unban(member)
+            await self.liara.say('Done. Good riddance.')
+        except discord.Forbidden:
+            await self.liara.say('Sorry, I don\'t have permission to ban people here.')
+
+    @commands.command(no_pm=True)
+    @checks.mod_or_permissions(kick_members=True)
+    async def kick(self, member: discord.Member):
+        """Kicks a member."""
+        try:
+            await self.liara.ban(member)
+            await self.liara.unban(member)
+            await self.liara.say('Done. Good riddance.')
+        except discord.Forbidden:
+            await self.liara.say('Sorry, I don\'t have permission to kick people here.')
 
 
 def setup(liara):
