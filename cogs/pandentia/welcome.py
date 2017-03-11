20eccf6da9c4b5af7c1a7e8825ef96ec0e4da1d3
Pandentia <hi@pandentia.cf>
2017-03-11 23:33:22 +0100

    Port welcome cog to rewrite

--- cogs/pandentia/welcome.py
+++ cogs/pandentia/welcome.py
@@ -22,28 +22,28 @@ class Welcome:
         else:
             return False
 
-    @commands.command(pass_context=True)
+    @commands.command()
     @checks.mod_or_permissions(administrator=True)
-    async def welcomeset(self, ctx, channel: discord.Channel, *, message: str):
+    async def welcomeset(self, ctx, channel, *, message: str):
         """Sets a welcome message for your current server."""
-        if channel.type != discord.ChannelType.text:
+        if not isinstance(channel, discord.TextChannel):
             await self.liara.say('That\'s not a text channel!')
             return
-        if len(message) > 500:
-            await self.liara.say('That welcome message is too long. Try something under 500 characters.')
+        if len(message) > 750:
+            await self.liara.say('That welcome message is too long. Try something under 750 characters.')
             return
-        server = str(ctx.message.server.id)
+        server = str(ctx.message.guild.id)
         self.check_for_server(server)
         self.welcome[server]['status'] = True
         self.welcome[server]['channel'] = str(channel.id)
         self.welcome[server]['message'] = message
-        await self.liara.say('Welcome message set.')
+        await ctx.send('Welcome message set.')
 
-    @commands.command(pass_context=True)
+    @commands.command()
     @checks.mod_or_permissions(administrator=True)
     async def welcomeclear(self, ctx):
         """Clears the server's welcome message."""
-        server = str(ctx.message.server.id)
+        server = str(ctx.message.guild.id)
         if self.check_for_server(server):
             self.welcome.pop(server)
             await self.liara.say('Welcome message cleared.')
@@ -51,16 +51,16 @@ class Welcome:
             await self.liara.say('This server doesn\'t have a welcome message.')
 
     async def on_member_join(self, member):
-        server = str(member.server.id)
+        server = str(member.guild.id)
         await asyncio.sleep(2)
         if self.disabled:
             return
         if self.check_for_server(server):
-            channel = self.liara.get_channel(self.welcome[server]['channel'])
+            channel = self.liara.get_channel(int(self.welcome[server]['channel']))
             if channel is None:
                 self.welcome.pop(server)
                 return
-            permissions = channel.permissions_for(member.server.me)
+            permissions = channel.permissions_for(member.guild.me)
             message = self.welcome[server]['message'].replace('%m', member.mention).replace('%n', member.name)
             if not permissions.send_messages:
                 self.welcome.pop(server)
@@ -71,7 +71,7 @@ class Welcome:
                 embed = discord.Embed()
                 embed.set_author(name=str(member), icon_url=member.avatar_url)
                 embed.description = message
-                await self.liara.send_message(channel, embed=embed)
+                await channel.send(embed=embed)
 
 
 def setup(liara):
