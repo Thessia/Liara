319b794845a74f99dbbb2e593c9cbb1192e4bb6d
Pandentia <hi@pandentia.cf>
2017-03-12 12:51:12 +0100

    Fix the welcome cog
    Oh dear.

--- cogs/pandentia/welcome.py
+++ cogs/pandentia/welcome.py
@@ -14,64 +14,61 @@ class Welcome:
     def __unload(self):
         self.welcome.die = True
 
-    def check_for_server(self, server_id):
-        if server_id not in self.welcome:
-            self.welcome[server_id] = {'status': False}
-        if self.welcome[server_id]['status']:
+    def check_for_guild(self, guild_id):
+        if self.welcome.get(guild_id, {'status': False})['status']:
             return True
         else:
             return False
 
     @commands.command()
     @checks.mod_or_permissions(administrator=True)
-    async def welcomeset(self, ctx, channel, *, message: str):
-        """Sets a welcome message for your current server."""
-        if not isinstance(channel, discord.TextChannel):
-            await self.liara.say('That\'s not a text channel!')
-            return
+    async def welcomeset(self, ctx, channel: discord.TextChannel, *, message: str):
+        """Sets a welcome message for your current server.
+        - channel: A text channel
+        - message: A markdown-formatted text message. use the %n and %m placeholders for
+        names and mentions respectively."""
         if len(message) > 750:
-            await self.liara.say('That welcome message is too long. Try something under 750 characters.')
+            await ctx.send('That welcome message is too long. Try something under 750 characters.')
             return
-        server = str(ctx.message.guild.id)
-        self.check_for_server(server)
-        self.welcome[server]['status'] = True
-        self.welcome[server]['channel'] = str(channel.id)
-        self.welcome[server]['message'] = message
+        guild = str(ctx.message.guild.id)
+        welcome_obj = {'status': True, 'channel': str(channel.id), 'message': message}
+        self.welcome[guild] = welcome_obj
         await ctx.send('Welcome message set.')
 
     @commands.command()
     @checks.mod_or_permissions(administrator=True)
     async def welcomeclear(self, ctx):
         """Clears the server's welcome message."""
-        server = str(ctx.message.guild.id)
-        if self.check_for_server(server):
-            self.welcome.pop(server)
-            await self.liara.say('Welcome message cleared.')
+        guild = str(ctx.message.guild.id)
+        if self.check_for_guild(guild):
+            self.welcome.pop(guild)
+            await ctx.send('Welcome message cleared.')
         else:
-            await self.liara.say('This server doesn\'t have a welcome message.')
+            await ctx.send('This server doesn\'t have a welcome message.')
 
     async def on_member_join(self, member):
-        server = str(member.guild.id)
+        guild = str(member.guild.id)
         await asyncio.sleep(2)
         if self.disabled:
             return
-        if self.check_for_server(server):
-            channel = self.liara.get_channel(int(self.welcome[server]['channel']))
-            if channel is None:
-                self.welcome.pop(server)
-                return
-            permissions = channel.permissions_for(member.guild.me)
-            message = self.welcome[server]['message'].replace('%m', member.mention).replace('%n', member.name)
-            if not permissions.send_messages:
-                self.welcome.pop(server)
-                return
-            elif not permissions.embed_links:
-                await self.liara.send_message(channel, message)
-            else:
-                embed = discord.Embed()
-                embed.set_author(name=str(member), icon_url=member.avatar_url)
-                embed.description = message
-                await channel.send(embed=embed)
+        if not self.check_for_guild(guild):
+            return
+        channel = self.liara.get_channel(int(self.welcome[guild]['channel']))
+        if channel is None:
+            self.welcome.pop(guild)
+            return
+        permissions = channel.permissions_for(member.guild.me)
+        message = self.welcome[guild]['message'].replace('%m', member.mention).replace('%n', member.name)
+        if not permissions.send_messages:
+            self.welcome.pop(guild)
+            return
+        elif not permissions.embed_links:
+            await self.liara.send_message(channel, message)
+        else:
+            embed = discord.Embed()
+            embed.set_author(name=str(member), icon_url=member.avatar_url)
+            embed.description = message
+            await channel.send(embed=embed)
 
 
 def setup(liara):
