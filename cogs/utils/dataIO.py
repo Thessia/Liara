ad1bc1595ef4d46422cc53003cf5e967cb0bbace
Pandentia <hi@pandentia.cf>
2017-05-01 15:29:19 +0200

    Tweak dataIO slightly to compare objects instead of pickled bytes
    (should fix dictionary ordering causing a loop between shards)

--- cogs/utils/dataIO.py
+++ cogs/utils/dataIO.py
@@ -1,6 +1,7 @@
 import threading
 import time
 import pickle
+import copy
 import json
 # noinspection PyUnresolvedReferences
 import __main__
@@ -49,7 +50,7 @@ class RedisDict(dict):
     def _loop(self):
         while not self.die:
             for item in self:
-                new = pickle.dumps(self.get(item))
+                new = copy.deepcopy(self.get(item))
                 old = self._modified.get(item)
 
                 if new != old:
