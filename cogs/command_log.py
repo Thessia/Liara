99c00b309e89ceb32528c217e308b91f94a56e2b
Pandentia <hi@pandentia.cf>
2017-06-27 22:08:20 +0200

    Make the command log more detailed

--- cogs/command_log.py
+++ cogs/command_log.py
@@ -7,7 +7,10 @@ class CommandLog:
         self.log = logging.getLogger('liara.command_log')
 
     async def on_command(self, ctx):
-        self.log.info('{0.author} ({0.author.id}) executed command "{0.command}" in {0.guild}'.format(ctx))
+        kwargs = ', '.join(['{}={}'.format(k, repr(v)) for k, v in ctx.kwargs.items()])
+        args = 'with arguments {} '.format(kwargs) if kwargs else ''
+        self.log.info('{0.author} ({0.author.id}) executed command "{0.command}" {1}in {0.guild} ({0.guild.id})'
+                      .format(ctx, args))
 
 
 def setup(liara):
