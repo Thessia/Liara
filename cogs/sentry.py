d907b4e5b01028ce48d33cba2fbef5efdb1ad7a4
Pandentia <hi@pandentia.qcx.io>
2018-03-23 12:37:57 +0100

    Fix the Sentry cog

--- cogs/sentry.py
+++ cogs/sentry.py
@@ -1,55 +1,58 @@
+from asyncio import Lock
+
 from discord.ext import commands
 from discord.ext.commands import errors as commands_errors
 from raven import Client as SentryClient
 from raven.exceptions import InvalidDsn
 
 from cogs.utils import checks
-from cogs.utils.storage import RedisDict
+from cogs.utils.storage import RedisCollection
 
 
 class Sentry:
     """A simple cog for bug reports."""
     def __init__(self, liara):
         self.liara = liara
-        self.settings = RedisDict('sentry', liara.redis)
-        if 'dsn' not in self.settings:
-            self.settings['dsn'] = None
-            self.settings.commit('dsn')
+        self.settings = RedisCollection(liara.redis, 'redis')
         self.client = None
-
-    def __unload(self):
-        self.settings.close()
+        self.client_lock = Lock(loop=self.liara.loop)
 
     async def on_command_error(self, exception, context):
+        # raven setup
         if self.client is None:
             self.client = SentryClient(site=self.liara.user.id)
+        dsn = await self.settings.get('dsn', None)
+        if dsn is None:
+            return
+        try:
+            self.client.set_dsn(dsn)
+        except InvalidDsn:
+            await self.settings.delete('dsn')
+            return
+
         if isinstance(exception, commands_errors.MissingRequiredArgument):
             return
         if isinstance(exception, commands_errors.CommandNotFound):
             return
         if isinstance(exception, commands_errors.BadArgument):
             return
-        if self.settings['dsn'] is None:
-            return
-        if context.command is self.liara.get_command('eval'):
-            return
-        try:
-            self.client.set_dsn(self.settings['dsn'])
-        except InvalidDsn:
-            self.settings['dsn'] = None
-            self.settings.commit('dsn')
-            self.client.set_dsn(None)
 
         _exception = exception.original
         message = context.message
-        self.client.user_context({'id': message.author.id})
-        # noinspection PyBroadException
-        try:
-            raise _exception
-        except:
-            self.client.captureException(data={'message': message.content}, extra={'guild_id': message.guild.id,
-                                                                                   'channel_id': message.channel.id,
-                                                                                   'message_id': message.id})
+        async with self.client_lock:
+            self.client.user_context({'id': message.author.id})
+            # noinspection PyBroadException
+            try:
+                raise _exception
+            except Exception:
+                self.client.captureException(
+                    data={'message': message.content},
+                    extra={
+                        'guild_id': message.guild.id,
+                        'channel_id': message.channel.id,
+                        'message_id': message.id
+                    }
+                )
 
     @commands.command()
     @checks.is_owner()
@@ -62,13 +65,12 @@ class Sentry:
         except InvalidDsn:
             await ctx.send('That DSN is invalid.')
             return
-        self.settings['dsn'] = dsn
-        self.settings.commit('dsn')
+        await self.settings.set('dsn', dsn)
         if dsn is None:
             await ctx.send('DSN cleared.')
         else:
             await ctx.send('DSN successfully set! All your exceptions will now be logged to Sentry.')
-            self.client.captureMessage('Hello, world!')
+            self.client.captureMessage('Sentry for Liara successfully set up.')
 
 
 def setup(liara):
